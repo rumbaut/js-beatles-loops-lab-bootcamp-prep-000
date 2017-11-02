5418148a44b9a02079e4ba48e961ecc97255a008
Fernando R González <rumbaut2288@gmail.com>
2017-11-02 19:38:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,17 +9,8 @@ function theBeatlesPlay(musicians, instruments){
 function johnLennonFacts(facts){
   for(var i = 0; i< facts.length; i++){
     facts[i] = facts[i] + "!!!";
-    console.log(facts[i]);
   }
   return facts;
 }
 
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
 
-johnLennonFacts(facts);
-console.log("hello");
