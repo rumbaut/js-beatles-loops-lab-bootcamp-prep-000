a1af0f41df46dcd84a756de8b4dd973953653df5
Fernando R González <rumbaut2288@gmail.com>
2017-11-02 19:33:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var ret = [];
-  while (musicians.lenght > 0) {
+  while (musicians.length > 0) {
     ret.push(musicians.pop() + " plays " + instruments.pop() );
   }
   return ret;
